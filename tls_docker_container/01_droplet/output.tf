13175f53edcef3acd1cb06d9f5bd25e4a2529f47
Joshua Shanks <jjshanks@gmail.com>
2020-05-31 09:46:10 -0700

    Rename first directory

--- tls_docker_container/01_droplet/output.tf
+++
@@ -1,7 +0,0 @@
-output "ssh_key_id" {
-  value = digitalocean_ssh_key.prod.id
-}
-
-output "docker01_ip" {
-  value = digitalocean_droplet.docker01.ipv4_address
-}
--- tls_docker_container/01_droplet/vars.tf
+++
@@ -1,3 +0,0 @@
-variable "do_region" {
-  default = "sfo2"
-}
